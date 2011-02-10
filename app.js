49491c9e90da2a4f2eba9455d89d9553c8eb5a17
Miles Wu <miles.wu@wu-home.co.uk>
2011-02-10 16:52:46 -0500

    Initial

---
+++ app.js
@@ -0,0 +1,51 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/nyaa2');
+var Torrent = require('./torrents.js').Torrent(db);
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyDecoder());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.logger());
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  Torrent.find({}, function(err, torrents) { 
+    res.render('browse', {
+      locals: {
+        title: 'Browsing All Torrents',
+        torrents: torrents
+      }
+    });
+  });
+});
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log("Express server listening on port %d", app.address().port)
+}
---
+++ torrents.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId;
+
+var torrentSchema = new Schema({
+  name : String,
+  uploader : ObjectID,
+  group : ObjectID,
+  date : { type: Date, default: Date.now},
+  filesize : Number,
+  snatched: Number
+  category : ObjectID
+
+
+
+});
+
+mongoose.model('Torrent', torrentSchema);
+
+exports.Torrent = function(db) {
+  return db.model('Torrent');
+};
+
